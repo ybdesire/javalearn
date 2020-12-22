0cdc7aa0820c91cc2875f98027566eb16f90d1d1
ybdesire <ybdesire@gmail.com>
2020-12-22 16:33:32 +0800

    Add files via upload

---
+++ 01_basic_knowledge/11_file_op/read_txt_parse_by_comma/MyParse.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class MyParse
+{
+    public static void main(String[] args)
+    {
+        try  
+        {  
+            File file=new File("data.txt");    //creates a new file instance  
+            FileReader fr=new FileReader(file);   //reads the file  
+            BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream  
+            StringBuffer sb=new StringBuffer();    //constructs a string buffer with no characters  
+            String line;  
+            while((line=br.readLine())!=null)  
+            {  
+                sb.append(line);      //appends line to string buffer  
+                sb.append("\n");     //line feed   
+                String[] tokens = line.split("; ");
+                for(int i=0;i<tokens.length;i++)
+                {
+                    if(tokens[i].contains("AAAAA"))
+                        System.out.println(tokens[i]);
+                }
+            }  
+            fr.close();    //closes the stream and release the resources  
+            //System.out.println("Contents of File: ");  
+            //System.out.println(sb.toString());   //returns a string that textually represents the object  
+        }  
+        catch(IOException e)  
+        {  
+            e.printStackTrace();  
+        }  
+
+    }
+}
